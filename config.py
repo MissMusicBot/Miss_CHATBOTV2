8af8e02e6f63c7752f87415d9d27dfe6a596106e
MissMusicBot <khatriuzef197@gmail.com>
2025-07-23 23:12:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "26504251"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "a50070614b2e87e3a0f2cd3ae08034c9"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7995626429"))
+OWNER_ID = int(getenv("OWNER_ID", "8130271170"))
 SUPPORT_GRP = "ARISHFA_UPDATE"
 UPDATE_CHNL = "THUNDERDEVS"
-OWNER_USERNAME = "ll_ISTKHAR_BABY_lll"
+OWNER_USERNAME = "System0999"
